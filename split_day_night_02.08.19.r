69f9b3b13faa1a123633bdc773718de062d4fa43
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-10 20:44:16 +0100

    a lot of unsuccessful tries

--- split_day_night_02.08.19.r
+++ split_day_night_02.08.19.r
@@ -15,6 +15,9 @@ str(sun2)
 sun2$sunrise_wDawn=sun2$sunrise+0.5*60*60 #add 30 min dawn
 sun2$sunset_wDawn=sun2$sunset-0.5*60*60 #substract 30min dawn
 
+#use start_time and end_time to subset sun2 data to correct length
+sun2=sun2[sun2$date>=start_time&sun2$date<=end_time,]
+
 #create column with only date in temperature
 # select first column and write into seperate list
 list_iButton_corr_tidy_col <- lapply(list_iButton_corr_tidy, `[`, 1)
@@ -27,11 +30,30 @@ list_iButton_corr_tidy_date <- mapply(cbind, list_iButton_corr_tidy, "Date"=list
 #filter temp data for range between sunrise (+ 30 min) and sunset (-30 min)  to include dawn
 
 #test with one dataframe
-test_day=list_iButton_corr_tidy
-test_day=list_iButton_corr_tidy[[1]]
 
-str(test_day)
-test_day=test_day[test_day$Datetime.1]
+test=list_iButton_corr_tidy_date[[1]]
+str(test)
+
+test_day=test[test$Date==sun2$date[1],]
+test_day=test_day[test_day$Datetime.1>=sun2$sunrise_wDawn[sun2$date=="2019-08-03 BST"]&test_day$Datetime.1<=sun2$sunset_wDawn[sun2$date=="2019-08-03 BST"],]
+test[test$Date==sun2$date[1],]=subset(test_day, Datetime.1>=sun2$sunrise_wDawn[sun2$date=="2019-08-03 BST"]&Datetime.1<=sun2$sunset_wDawn[sun2$date=="2019-08-03 BST"])
+sub=subset(test_day, Datetime.1>=sun2$sunrise_wDawn[sun2$date=="2019-08-03 BST"]&Datetime.1<=sun2$sunset_wDawn[sun2$date=="2019-08-03 BST"])
+
+test=test[test$Date==sun2$date[1]&test$Datetime.1>=sun2$sunrise_wDawn[sun2$date=="2019-08-03 BST"]&test$Datetime.1<=sun2$sunset_wDawn[sun2$date=="2019-08-03 BST"],]
+#str
+                                    
+#test[test$Date==sun2$date[1],]=paste(test_day, rep(NA, length(test$Temperature_C[test$Date==sun2$date[1]])-length(test_day$Temperature_C)))
+#str(test_day)
+#dim(test[test$Date==sun2$date[1],])
+#dim(test_day)
+
+for(i in sun2$date)
+test_day=test[test$Date==i,] #subset the day that matches i from sun from dataframe
+test_day=test_day[test_day$] #subset the day with sunrise and sunset value from sun for i
+test[test$Date==i,]=test_day #replace orinigal day with shortend day and fill with NAs
+test_day=test$Temperature_C[test$Date==sun2$date[5]]
+
+
 
 for (i in 1:length(test_day)) {
   name_save=names(test_day)
