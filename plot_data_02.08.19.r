7e6cdf44c43bb9e6f3b6dd09adf3faebced8fc15
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-12 15:25:31 +0200

    plot and save data for every logger

---
+++ plot_data_02.08.19.r
@@ -0,0 +1,37 @@
+#plot all the plots for cleared and split data
+setwd("C:/00 Dana/Uni/6. Semester/Bachelorarbeit/plots_day_night")
+
+#plot and save day data
+
+for(i in 1:length(list_iButton_corr_tidy_date_day)){
+  dataname=names(list_iButton_corr_tidy_date_day)[i]
+  name=paste("day","plot",dataname,".jpg")
+  title=paste("Day Temperature",dataname)
+  data=list_iButton_corr_tidy_date_day[[i]]
+  jpeg(filename = name)
+  plot(data$Datetime.1, data$Temperature_C, main=title)
+  abline(v=sun2$sunrise, col="blue")
+  abline(v=sun2$sunset, col="red")
+  legend("topright", 
+         legend=c("Day Temperature", "Sunrise", "Sunset"), 
+         fill=c("black", "orange", "red"), cex=0.7)
+  dev.off()
+  }
+
+#plot and save night data
+
+for(i in 1:length(list_iButton_corr_tidy_date_night)){
+  dataname=names(list_iButton_corr_tidy_date_night)[i]
+  name=paste("night","plot",dataname,".jpg")
+  title=paste("Night Temperature",dataname)
+  data=list_iButton_corr_tidy_date_night[[i]]
+  jpeg(filename = name)
+  plot(data$Datetime.1, data$Temperature_C, main=title)
+  abline(v=sun2$sunrise, col="blue")
+  abline(v=sun2$sunset, col="red")
+  legend("topright", 
+         legend=c("Night Temperature", "Sunrise", "Sunset"), 
+         fill=c("black", "orange", "red"), cex=0.7)
+  dev.off()
+}
+
