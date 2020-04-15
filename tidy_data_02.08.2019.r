3dbd1d869946f754ac9d12f4e5e1c6c01ec0fd5a
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-15 16:23:50 +0200

    write loop to add offset to temperature

--- tidy_data_02.08.2019.r
+++ tidy_data_02.08.2019.r
@@ -3,23 +3,24 @@
 setwd("C:/00 Dana/Uni/6. Semester/Bachelorarbeit")
 
 str(list_iButton_corr) #used from iButtons-check script (check that date corresponds)
-
+#create temporary list
+list_iButton_corr_temp=list_iButton_corr
 #read in offset values
 offset_stats=read.table("iButton_Statistics.csv", sep=",", dec=".", header=T)
 offset_stats=offset_stats[1:107,] #remove columns with NA
 
 #test with one dataframe:
-str(offset_stats)
-off_value=offset_stats$T_offset[offset_stats$ID_iButton=="100"]
+#str(offset_stats)
+#off_value=offset_stats$T_offset[offset_stats$ID_iButton=="100"]
 
 #try if indexing with int number works
-ztest=list_iButton_corr[["100"]]
-ztest$Temperature_C=ztest$Temperature_C+off_value
+#ztest=list_iButton_corr[["100"]]
+#ztest$Temperature_C=ztest$Temperature_C+off_value
 
 list_iButton_corr[[1]]=NULL
 
 for (i in names(list_iButton_corr)){
-  if(exists(x = i, where=offset_stats$ID_iButton){
+  if(any(offset_stats$ID_iButton==i)){
   off_value=offset_stats$T_offset[offset_stats$ID_iButton==i]
   data=list_iButton_corr[[i]]
   data$Temperature_C_w_off=data$Temperature_C+off_value
@@ -32,11 +33,21 @@ for (i in names(list_iButton_corr)){
 }
 #number 99 exists two times?
 #use for loop to assign offset to name?
-off_value=offset_stats$T_offset[offset_stats$ID_iButton=="94"]
-data=list_iButton_corr[["94"]]
-data$Temperature_C_w_off=data$Temperature_C+off_value
-list_iButton_corr[["94"]]=data
-
+i="10"
+if(any(offset_stats$ID_iButton==i)){
+  off_value=offset_stats$T_offset[offset_stats$ID_iButton==i]
+  data=list_iButton_corr[[i]]
+  data$Temperature_C_w_off=data$Temperature_C+off_value
+  list_iButton_corr[[i]]=data
+} else{
+  data=list_iButton_corr[[i]]
+  data$Temperature_C_miss_off=data$Temperature_C
+  list_iButton_corr[[i]]=data
+}
+  
+?exists
+?any
+  ))}}
 #iterate through all dataframes on list
 #iterate trough all temperature values 
 #set threshold value corresponding to literature regarding spikes
