31c3ab66dfa760c6a54e5294601c04e4455a0df9
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-15 15:41:05 +0200

    add offset of logger to temperature data

--- tidy_data_02.08.2019.r
+++ tidy_data_02.08.2019.r
@@ -6,11 +6,36 @@ str(list_iButton_corr) #used from iButtons-check script (check that date corresp
 
 #read in offset values
 offset_stats=read.table("iButton_Statistics.csv", sep=",", dec=".", header=T)
+offset_stats=offset_stats[1:107,] #remove columns with NA
+
+#test with one dataframe:
 str(offset_stats)
-name=names(list_iButton_corr)
-name
-offset_stats$Name_iButton
+off_value=offset_stats$T_offset[offset_stats$ID_iButton=="100"]
+
+#try if indexing with int number works
+ztest=list_iButton_corr[["100"]]
+ztest$Temperature_C=ztest$Temperature_C+off_value
+
+list_iButton_corr[[1]]=NULL
+
+for (i in names(list_iButton_corr)){
+  if(exists(x = i, where=offset_stats$ID_iButton){
+  off_value=offset_stats$T_offset[offset_stats$ID_iButton==i]
+  data=list_iButton_corr[[i]]
+  data$Temperature_C_w_off=data$Temperature_C+off_value
+  list_iButton_corr[[i]]=data
+  } else{
+    data=list_iButton_corr[[i]]
+    data$Temperature_C_miss_off=data$Temperature_C
+    list_iButton_corr[[i]]=data
+  }
+}
+#number 99 exists two times?
 #use for loop to assign offset to name?
+off_value=offset_stats$T_offset[offset_stats$ID_iButton=="94"]
+data=list_iButton_corr[["94"]]
+data$Temperature_C_w_off=data$Temperature_C+off_value
+list_iButton_corr[["94"]]=data
 
 #iterate through all dataframes on list
 #iterate trough all temperature values 
