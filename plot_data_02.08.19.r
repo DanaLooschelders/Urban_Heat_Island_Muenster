a745458c83f6d55f2ea422f835727f461d78d90b
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-18 15:49:17 +0200

    update column reference

--- plot_data_02.08.19.r
+++ plot_data_02.08.19.r
@@ -1,5 +1,5 @@
 #plot all the plots for cleared and split data
-setwd("C:/00 Dana/Uni/6. Semester/Bachelorarbeit/plots_day_night_01.08/")
+setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit/plots_day_night_01.08/")
 setwd("C:/00 Dana/Uni/6. Semester/Bachelorarbeit/plots_day_night_02.09/")
 #plot and save day data
 
@@ -9,7 +9,7 @@ for(i in 1:length(list_iButton_corr_tidy_date_day)){
   title=paste("Day Temperature",dataname)
   data=list_iButton_corr_tidy_date_day[[i]]
   jpeg(filename = name)
-  plot(data$Datetime.1, data$Temperature_C, main=title)
+  plot(data$Datetime.1, data[,4], main=title)
   abline(v=sun2$sunrise, col="blue")
   abline(v=sun2$sunset, col="red")
   legend("topright", 
@@ -26,7 +26,7 @@ for(i in 1:length(list_iButton_corr_tidy_date_night)){
   title=paste("Night Temperature",dataname)
   data=list_iButton_corr_tidy_date_night[[i]]
   jpeg(filename = name)
-  plot(data$Datetime.1, data$Temperature_C, main=title)
+  plot(data$Datetime.1, data[,4], main=title)
   abline(v=sun2$sunrise, col="blue")
   abline(v=sun2$sunset, col="red")
   legend("topright", 
