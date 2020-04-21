f990dc1f0446baac5686e98b06fc16f4ae510c05
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-21 09:38:41 +0200

    add color to legend

--- plot_data_in_pairs.R
+++ plot_data_in_pairs.R
@@ -37,7 +37,7 @@ metadata$color[metadata$type=="Vegetation"]="green"
 #write seperate lists for each place_ID (with for loop?)
 #then plot pairs depending on number of dataframes in list
 #add color depending on type of place 
-
+setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit/paired_plots/")
 setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit/paired_plots_02.09/")
 setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit/paired_plots_20.08/")
 setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit/paired_plots_14.08/")
@@ -53,13 +53,14 @@ for (i in unique(metadata$PlaceID)){
     dat_logger3=list_iButton_corr_tidy[names(list_iButton_corr_tidy)==Loggers[3]]
     plot(dat_logger1[[1]]$Datetime.1, dat_logger1[[1]][,4], 
          type="l", ylim=c(10,40), ylab="Temperature [°C]", xlab="Date",
-         col=metadata$color[metadata$ID==Loggers[1]])
+         col=metadata$color[metadata$ID==Loggers[1]],
+         main=paste("Plot", i))
     lines(dat_logger2[[1]]$Datetime.1, dat_logger2[[1]][,4],
           col=metadata$color[metadata$ID==Loggers[2]])
     lines(dat_logger3[[1]]$Datetime.1, dat_logger3[[1]][,4], 
           col=metadata$color[metadata$ID==Loggers[3]])
     legend("topright", legend=c("Settlement", "Vegetation", "Water"), 
-           col = c("darkgrey", "green", "blue"))
+           fill = c("darkgrey", "green", "blue"))
     dev.off()
     }
     else if(length(Loggers)==2){
@@ -73,27 +74,13 @@ for (i in unique(metadata$PlaceID)){
       dat_logger2=list_iButton_corr_tidy[names(list_iButton_corr_tidy)==Loggers[2]]
       plot(dat_logger1[[1]]$Datetime.1, dat_logger1[[1]][,4], 
            type="l", ylim=c(10,40), ylab="Temperature [°C]", xlab="Date",
-           col=metadata$color[metadata$ID==Loggers[1]])
+           col=metadata$color[metadata$ID==Loggers[1]],
+           main=paste("Plot", i))
       lines(dat_logger2[[1]]$Datetime.1, dat_logger2[[1]][,4],
             col=metadata$color[metadata$ID==Loggers[2]])
-      legend("topright", legend=c(label1, label2), col=c(col1, col2))
+      legend("topright", legend=c(label1, label2), fill=c(col1, col2))
       dev.off()
     }
   else{}
   
 }
-Loggers=metadata$ID[metadata$PlaceID=="Aasee_2"]
-col1=metadata$color[metadata$ID==Loggers[1]]
-col2=metadata$color[metadata$ID==Loggers[2]]
-label1=metadata$type[metadata$ID==Loggers[1]]
-label2=metadata$type[metadata$ID==Loggers[2]]
-dat_logger1=list_iButton_corr_tidy[names(list_iButton_corr_tidy)==Loggers[1]]
-dat_logger2=list_iButton_corr_tidy[names(list_iButton_corr_tidy)==Loggers[2]]
-plot(dat_logger1[[1]]$Datetime.1, dat_logger1[[1]][,4],
-     type="l", ylim=c(10,35), ylab="Temperature [°C]", xlab="Date",
-     col=metadata$color[metadata$ID==Loggers[1]])
-lines(dat_logger2[[1]]$Datetime.1, dat_logger2[[1]][,4],
-      col=metadata$color[metadata$ID==Loggers[2]])
-legend("topright", legend=c(label1, label2), col=c(col1, col2))
-Loggers
-col1
