9c7e2097cd30a1f0865f10e51f2da549e6454d8f
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-22 12:07:21 +0200

    differntiate for air temp and water temp spikes

--- tidy_data_08_and_09.r
+++ tidy_data_08_and_09.r
@@ -27,24 +27,41 @@ for (i in names(list_iButton_corr)){
 #iterate through all dataframes on list
 #iterate trough all temperature values 
 #set threshold value corresponding to literature regarding spikes
-#if difference between two values is larger then threshold -> set to NA
-
+#if difference between two values is larger then threshold -> set to NA for a defined timespan
+#assign description for plots
 
 list_iButton_corr_tidy=list_iButton_corr #create new, tidy list
 str(list_iButton_corr)
 report.na=rep(NA, length(list_iButton_corr_tidy))
-#replace all temperature spikes (rise in >5°C in 10 mins) by NAs and report missing values
-#for (i in 1:length(list_iButton_corr_tidy)) {
-  #name_save=names(list_iButton_corr_tidy[])
- # test=list_iButton_corr_tidy[[i]]
-  #test$diff=rep(NA)
-  #test$diff[1:length(test$Temperature_C_w_off)-1]=diff(test$Temperature_C_w_off)
-  #test$Temperature_C_w_off[test$diff>=5]=NA
-  #test$Temperature_C_w_off[test$diff<= -5]=NA
-  #list_iButton_corr_tidy[[i]]=test
-  #names(list_iButton_corr_tidy[])=name_save
-  #report.na[i]=sum(is.na(test$Temperature_C_miss_off))
-#}
+
+setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit")
+#read in csv with lat lon and description of places
+des=read.table("Lat_Lon_Logger.csv", sep=";", dec=",", header=T)
+des=des[,1:7] #drop column with water depth (for now)
+des=na.omit(des) #drop rows with NA
+des$ï..Logger.ID=as.character(des$ï..Logger.ID) #set logger ID as character to match names
+des$place_ID=paste(des$Place_name,des$Place_number, sep="_")
+str(des)
+ID=unique(des$place_ID)
+
+#create a metadata table for logger
+metadata=data.frame("ID"=as.integer(names(list_iButton_corr_tidy)))
+metadata=na.omit(metadata)
+metadata$PlaceID=rep(NA)
+metadata$type=rep(NA)
+for(i in metadata$ID){
+  if(any(des$ï..Logger.ID==i)) {
+    metadata$PlaceID[metadata$ID==i]=des$place_ID[des$ï..Logger.ID==i]
+    metadata$type[metadata$ID==i]=as.character(des$Place_type[des$ï..Logger.ID==i])
+  }else{}
+}
+metadata[is.na(metadata$PlaceID),2:3]="unknown"
+metadata$color=rep(NA)
+
+#set column with color for plots
+metadata$color[metadata$type=="Water"]="blue"
+metadata$color[metadata$type=="Sealed_area"]="darkgrey"
+metadata$color[metadata$type=="Vegetation"]="green"
 
 #-> use the second loop as one dataframe has values that are not corrected for the offset
 for (i in 1:length(list_iButton_corr_tidy)) {
@@ -63,4 +80,47 @@ report.na #check how many NAs were in data
 #for the 02.08.2019
 list_iButton_corr_tidy[["33"]] #somehow the offset for this one is missing
 
+#try new loop with longer removal and if loop
 
+for (i in 1:length(list_iButton_corr_tidy)) {
+  #name_save=names(list_iButton_corr_tidy[])
+  test=list_iButton_corr_tidy[[i]]
+  test$diff=rep(NA)
+  test$diff[1:length(test[,4])-1]=diff(test[,4])
+  spike=which(test$diff>=5)
+  for (x in spike) {
+    test[x:(x+18),4]=NA
+  }
+  test[,4][test$diff<= -5]=NA
+  list_iButton_corr_tidy[[i]]=test
+  #names(list_iButton_corr_tidy[])=name_save
+  report.na[i]=sum(is.na(test[,4]))
+}
+
+for (i in 1:length(list_iButton_corr_tidy)) {
+if (metadata$type[metadata$ID==i]=="Water") {
+test=list_iButton_corr_tidy[[i]]
+test$diff=rep(NA)
+test$diff[1:length(test[,4])-1]=diff(test[,4])
+spike=which(test$diff>=3) #3°C/10min as threshold after spike
+for (x in spike) {
+  test[x:(x+18),4]=NA #two hours of data after spike is removed
+}
+test[,4][test$diff<= -5]=NA
+list_iButton_corr_tidy[[i]]=test
+#names(list_iButton_corr_tidy[])=name_save
+report.na[i]=sum(is.na(test[,4]))
+} else { #for air temperature (sealed area and vegetation)
+  test=list_iButton_corr_tidy[[i]]
+  test$diff=rep(NA)
+  test$diff[1:length(test[,4])-1]=diff(test[,4])
+  spike=which(test$diff>=5) #5°C/10min as threshold for spike
+  for (x in spike) { #half an hour of data after spike is removed
+    test[x:(x+3),4]=NA
+  }
+  test[,4][test$diff<= -5]=NA
+  list_iButton_corr_tidy[[i]]=test
+  #names(list_iButton_corr_tidy[])=name_save
+  report.na[i]=sum(is.na(test[,4]))
+}
+}
