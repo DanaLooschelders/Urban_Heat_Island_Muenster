061c2ceea7e25d4e7561b27998d40ec8ebd14b6d
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-23 22:00:28 +0200

    loop kinda working

--- start_time_correction.R
+++ start_time_correction.R
@@ -11,9 +11,9 @@ date_time_complete <- seq.POSIXt(from=start_time,
 str(date_time_complete)
 str(test)
 
-test=xts(list_iButton_corr[[1]][,4],list_iButton_corr[[1]][,3])
+test=xts(list_iButton_corr[[1]][,2],list_iButton_corr[[1]][,3])
 
-test2 = merge(test,xts(,date_time_complete))
+test2 = merge(test,date_time_complete)
 test2=na.approx(test2)
 test2=as.data.frame(test2)
 test2$Datetime.1=rownames(test2)
@@ -23,14 +23,19 @@ test3=test2[c(rep(FALSE,9),TRUE), ]
 #loop through list
 date_time_complete <- seq.POSIXt(from=start_time,
                                  to=end_time,by="min") #create minute timeframe
-list_iButton_corr_set=list_iButton_corr #create new list to use as output
+list_iButton_corr_set=list_iButton_corr
+list_iButton_corr_set=lapply(list_iButton_corr_set, `[`, 2:3) #use only 2nd and 3rd column
+#create new list to use as output
 for(i in 1:length(list_iButton_corr)){
-  test=xts(list_iButton_corr[[i]][,4],list_iButton_corr[[i]][,3])
-  test2=merge(test,xts(,date_time_complete))
+  test=xts(list_iButton_corr[[i]][,2],list_iButton_corr[[i]][,3])
+  test2=merge(test,date_time_complete)
   test2=na.approx(test2)
-  test2=as.data.frame("Temperature_corr"=test2)
+  test2=data.frame("Temperature_corr"=test2)
   test2$Datetime.1=rownames(test2)
   rownames(test2)=NULL
   test3=test2[c(rep(FALSE,9),TRUE),]
-  list_iButton_corr_set[[i]][,]
+  list_iButton_corr_set[[i]][1:length(test3[,1]),1:2]=test3[,1:2]
+  list_iButton_corr_set[[i]][length(test3[,1]+1),1:2]=NA
 }
+#eventuell if Schleife einbauen, 
+#falls nicht zwangsläufig der letzte Wert ersetzt werden muss
