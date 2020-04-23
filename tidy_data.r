feab61fbcbc40e9640948394bf561d31c08d6f21
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-23 22:29:02 +0200

    change col ref and list

--- tidy_data.r
+++ tidy_data.r
@@ -3,23 +3,23 @@
 #and for 01.09.2019
 setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit")
 
-str(list_iButton_corr) #used from iButtons-check script (check that date corresponds)
+str(list_iButton_corr_set) #the start-time corrected data (check that date corresponds)
 #create temporary list
-list_iButton_corr_temp=list_iButton_corr
+list_iButton_corr_temp=list_iButton_corr_set
 #read in offset values
 offset_stats=read.table("iButton_Statistics.csv", sep=",", dec=".", header=T)
 str(offset_stats)
 
-for (i in names(list_iButton_corr)){
+for (i in names(list_iButton_corr_set)){
   if(any(offset_stats$ID_iButton==i)){
   off_value=offset_stats$Diff_T[offset_stats$ID_iButton==i]
-  data=list_iButton_corr[[i]]
+  data=list_iButton_corr_set[[i]]
   data$Temperature_C_w_off=data$Temperature_C+off_value
-  list_iButton_corr[[i]]=data
+  list_iButton_corr_set[[i]]=data
   } else{
-    data=list_iButton_corr[[i]]
+    data=list_iButton_corr_set[[i]]
     data$Temperature_C_miss_off=data$Temperature_C
-    list_iButton_corr[[i]]=data
+    list_iButton_corr_set[[i]]=data
   }
 }
 
@@ -33,7 +33,7 @@ for (i in names(list_iButton_corr)){
 #if difference between two values is larger then threshold -> set to NA for a defined timespan
 #assign description for plots
 
-list_iButton_corr_tidy=list_iButton_corr #create new, tidy list
+list_iButton_corr_tidy=list_iButton_corr_set #create new, tidy list
 str(list_iButton_corr)
 report.na=rep(NA, length(list_iButton_corr_tidy))
 
@@ -72,27 +72,27 @@ for (i in 1:length(list_iButton_corr_tidy)) {
 if (metadata$type[metadata$ID==name]=="Water") {
 test=list_iButton_corr_tidy[[i]]
 test$diff=rep(NA)
-test$diff[1:length(test[,4])-1]=diff(test[,4])
+test$diff[1:length(test[,3])-1]=diff(test[,3])
 spike=which(test$diff>=3) #3°C/10min as threshold after spike
 for (x in spike) {
-  test[x:(x+18),4]=NA #two hours of data after spike is removed
+  test[x:(x+18),3]=NA #two hours of data after spike is removed
 }
-test[,4][test$diff<= -5]=NA
+test[,3][test$diff<= -5]=NA
 list_iButton_corr_tidy[[i]]=test
 #names(list_iButton_corr_tidy[])=name_save
-report.na[i]=sum(is.na(test[,4]))
+report.na[i]=sum(is.na(test[,3]))
 } else { #for air temperature (sealed area and vegetation)
   test=list_iButton_corr_tidy[[i]]
   test$diff=rep(NA)
-  test$diff[1:length(test[,4])-1]=diff(test[,4])
+  test$diff[1:length(test[,3])-1]=diff(test[,3])
   spike=which(test$diff>=5) #5°C/10min as threshold for spike
   for (x in spike) { #half an hour of data after spike is removed
     test[x:(x+3),4]=NA
   }
-  test[,4][test$diff<= -5]=NA
+  test[,3][test$diff<= -5]=NA
   list_iButton_corr_tidy[[i]]=test
   #names(list_iButton_corr_tidy[])=name_save
-  report.na[i]=sum(is.na(test[,4]))
+  report.na[i]=sum(is.na(test[,3]))
 }
 }
 
