f597c8016cdc1ca021e09aaeb4b45c992c9e5dad
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-23 14:45:34 +0200

    start loop

--- start_time_correction_interpolate_data.R
+++ start_time_correction_interpolate_data.R
@@ -19,3 +19,18 @@ test2=as.data.frame(test2)
 test2$Datetime.1=rownames(test2)
 rownames(test2)=NULL
 test3=test2[c(rep(FALSE,9),TRUE), ]
+
+#loop through list
+date_time_complete <- seq.POSIXt(from=start_time,
+                                 to=end_time,by="min") #create minute timeframe
+list_iButton_corr_set=list_iButton_corr #create new list to use as output
+for(i in 1:length(list_iButton_corr)){
+  test=xts(list_iButton_corr[[i]][,4],list_iButton_corr[[i]][,3])
+  test2=merge(test,xts(,date_time_complete))
+  test2=na.approx(test2)
+  test2=as.data.frame("Temperature_corr"=test2)
+  test2$Datetime.1=rownames(test2)
+  rownames(test2)=NULL
+  test3=test2[c(rep(FALSE,9),TRUE),]
+  list_iButton_corr_set[[i]][,]
+}
