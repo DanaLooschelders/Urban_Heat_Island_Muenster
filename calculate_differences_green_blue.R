67ac24cc059bc319b3150330ddb1d871aa8a6eee
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-23 11:12:26 +0200

    partly working for loop

--- calculate_differences_green_blue.R
+++ calculate_differences_green_blue.R
@@ -1,33 +1,25 @@
 #plot the differences between sealed areas and vegetation
 i="Kanonengraben_1"
-for (i in unique(metadata$PlaceID)){
-  dataname=i #save logger ID 
-  name=paste("difference","plot",dataname,".pdf")
-  Loggers=metadata$ID[metadata$PlaceID==i]
-  if(any(metadata$type[metadata$PlaceID==i]=="Vegetation")&any(metadata$type[metadata$PlaceID=="Kanonengraben_1"]=="Sealed_area")){
-    vegetation=Loggers[Loggers==metadata$ID[metadata$PlaceID==i&metadata$type=="Vegetation"]]
-    sealed=Loggers[Loggers==metadata$ID[metadata$PlaceID==i&metadata$type=="Sealed_area"]]
-    dat_logger_veg=list_iButton_corr_tidy[names(list_iButton_corr_tidy)==Loggers[vegetation]]
-    dat_logger_sealed= dat_logger_veg=list_iButton_corr_tidy[names(list_iButton_corr_tidy)==Loggers[sealed]]
-    diff_temp=dat_logger
-     }
-    pdf(file=name, paper = "a4r", height=7, width=14)
-    dat_logger1=list_iButton_corr_tidy[names(list_iButton_corr_tidy)==Loggers[1]]
-    dat_logger2=list_iButton_corr_tidy[names(list_iButton_corr_tidy)==Loggers[2]]
-    dat_logger3=list_iButton_corr_tidy[names(list_iButton_corr_tidy)==Loggers[3]]
-    water_logger=metadata$ID[metadata$PlaceID==i&metadata$type=="Vegetation"]
-    plot(dat_logger1[[1]]$Datetime.1, dat_logger1[[1]][,4], 
-         type="l", ylim=c(10,45), ylab="Temperature [°C]", xlab="Date",
-         col=metadata$color[metadata$ID==Loggers[1]],
-         main=paste("Plot", i))
-    lines(dat_logger2[[1]]$Datetime.1, dat_logger2[[1]][,4],
-          col=metadata$color[metadata$ID==Loggers[2]])
-    lines(dat_logger3[[1]]$Datetime.1, dat_logger3[[1]][,4], 
-          col=metadata$color[metadata$ID==Loggers[3]])
-    legend("topright", legend=c("Sealed_area", "Vegetation", "Water"), 
-           fill = c("darkgrey", "green", "blue"))
-    dev.off()
-    
-    getwd()
+setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit/difference_plots_14.08")
 
-list_iButton_corr_tidy
+for (i in unique(metadata$PlaceID)){
+  dataname=i #save place ID 
+  name=paste("difference","plot",dataname,".pdf")
+  Loggers=metadata$ID[metadata$PlaceID==i] #save ID of Loggers belonging to place
+  #check if place has logger in grey and green infrastructure
+  if(any(metadata$type[metadata$PlaceID==i]=="Vegetation")&any(metadata$type[metadata$PlaceID=="Kanonengraben_1"]=="Sealed_area")){ 
+    vegetation=Loggers[Loggers==metadata$ID[metadata$PlaceID==i&metadata$type=="Vegetation"]] #get ID of logger in veg
+    sealed=Loggers[Loggers==metadata$ID[metadata$PlaceID==i&metadata$type=="Sealed_area"]] #get ID of logger in sealed area
+    dat_logger_veg=data.frame(list_iButton_corr_tidy[names(list_iButton_corr_tidy)==Loggers[Loggers==vegetation]]) #get veg logger data
+    dat_logger_sealed=data.frame(list_iButton_corr_tidy[names(list_iButton_corr_tidy)==Loggers[Loggers==sealed]]) #get sealed logger data
+    diff_temp=dat_logger_sealed[,4]-dat_logger_veg[,4] #calculate difference between grey and green infrastructure
+    pdf(file=name, paper = "a4r", height=7, width=14) #create pdf
+    plot(dat_logger_veg[,3], diff_temp, #plot difference
+         type="l", ylab="Temperature difference [°C]", xlab="Date",
+         main=paste("Temperature Difference between grey and green Infrastructure in", i))
+    dev.off()
+  }else{}
+}
+
+    str(dat_logger_sealed)
+str(dat_logger_veg)
