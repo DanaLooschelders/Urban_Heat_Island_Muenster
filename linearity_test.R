4c90f1de14a7ac19f38473fc02901bc8a57394bb
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-25 11:29:56 +0200

    decompose time series

--- linearity_test.R
+++ linearity_test.R
@@ -2,27 +2,54 @@
 #de-seasonalzse the data
 #then test for linearity
 
-require(tseries)
-library(forecast)
-?ts
-test=list_iButton_corr_tidy[[1]]
-testdata=list_iButton_corr_tidy[[1]][,4]
-range=range(list_iButton_corr_tidy[[1]][,3])
-ts(data=testdata, start = start_time, end = end_time, frequency=1)
-time_series=ts(testdata,start=c(2019,10),frequency=24*6)
-time_series=ts(testdata, start=c(14), frequency = 24*6)
-plot(time_series)
-
-#set frequency to 24*60 to get a daily seasonality
-#number of days into 2019
-str(time_series)
-des_ts=seasadj(time_series)
 #try reading in one series as test
 #use raw but tidy data (without offset correction)
+require(tseries)
+library(forecast)
+test=list_iButton_corr_tidy[[1]]
+testdata=list_iButton_corr_tidy[[1]][,4]
+time_series=ts(testdata, start=c(14), frequency = 24*6)
+  #frequency -> 24*6 for daily seasonality (10 min data)
+  #start 14 -> for 14th August 2019
+  #change start time to day of the year (1 to 365)
+plot(time_series)
+str(time_series)
+#decompose time series
+time_series_decomp=stl(time_series, "periodic", na.action = na.exclude)
 
-ts.stl <- stl(TS,"periodic")  # decompose the TS
-ts.sa <- seasadj(ts.stl)  # de-seasonalize
-plot(AirPassengers, type="l")  # original series
-plot(ts.sa, type="l")  # seasonal adjusted
-seasonplot(ts.sa, 12, col=rainbow(12), 
-           year.labels=TRUE, main="Seasonal plot: Airpassengers") # seasonal frequency set as 12 for monthly data.
+
+#try with raw data (only offset corrected, but with spikes)
+testdata=list_iButton_corr[[1]][,4] #use one temperature series as test
+time_series=ts(testdata, start=c(14), frequency = 24*6) #create time series
+plot(time_series) #plot ts
+any(is.na(time_series)) #check for na -> no missing values
+time_series_decomp=stl(time_series, "periodic") #decompose ts
+plot(time_series_decomp) #plot decomp ts
+
+times_series_decomp2=decompose(time_series) #decomose ts with another function
+plot(times_series_decomp2) #plot decomp ts
+
+time_series_sa=seasadj(time_series_decomp) #adjust for seasonality
+plot(time_series_sa) #plot deseasonlized ts
+
+#run decomposition for all data
+str(list_iButton_corr)
+#create column for julien day
+day=lapply(list_iButton_corr, '[',3)
+day_of_year=lapply(day, function (x) as.integer(strftime(x$Datetime.1, format = "%j")))
+list_iButton_corr <- mapply(cbind, list_iButton_corr, "Day"=day_of_year, SIMPLIFY=F)
+
+setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit/plots_ts_14.08")
+
+for (i in 1:length(list_iButton_corr)){
+  if (!any(is.na(list_iButton_corr[[i]][,4]))){
+  dataname=names(list_iButton_corr)[i] #save logger ID 
+  name=paste("time_series","plot",dataname,".pdf") #set filename
+  data=list_iButton_corr[[i]][,4] #retrieve column with temperature data from list
+  time_series=ts(data, start=c(list_iButton_corr[[i]][1,5]), frequency = 24*6) #create time series
+  time_series_decomp=stl(time_series, "periodic") #decompose ts
+  pdf(file = name, width=14, height=7, paper="a4r")
+  plot(time_series_decomp) #plot decomp ts
+  dev.off()
+}else{}
+}
