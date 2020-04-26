3d72396fa1434cdb9ec4ae9251a4f65fe4fed465
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-04-26 15:59:08 +0200

    rename column in metadata

--- water_data_plot-r.R
+++ water_data_plot-r.R
@@ -1,18 +1,18 @@
 #plot all water logger
 setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit")
 water=read.table("Water_logger_lat_lon.csv", sep=";", dec=".", header=T)
-
+names(water)[1]="Logger_ID"
 str(water)
 str(list_iButton_corr_tidy)
 
 #get index of all water loggers in dataset
-waterlogger=intersect(water$ï..Logger.ID, names(list_iButton_corr_tidy))
+waterlogger=intersect(water$Logger_ID, names(list_iButton_corr_tidy))
 list_iButton_corr_tidy_water=list() #create empty list with water logger
 #fill list with water logger data
 list_iButton_corr_tidy_water=list_iButton_corr_tidy[names(list_iButton_corr_tidy)%in%waterlogger] 
 #rename list
 names=names(list_iButton_corr_tidy_water)
-desc=water$Location_ID[water$ï..Logger.ID%in%names(list_iButton_corr_tidy_water)]
+desc=water$Location_ID[water$Logger_ID%in%names(list_iButton_corr_tidy_water)]
 names(list_iButton_corr_tidy_water)=paste(desc, " (",names, ")", sep="")
 names(list_iButton_corr_tidy_water) #check
 #plot all values as test
