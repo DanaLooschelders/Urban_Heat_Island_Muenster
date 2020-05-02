4f0ab1a7d91c2768562d665c27bbb931b0b71559
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-05-02 23:57:24 +0200

    subset stations to MS shapefile

--- data_from_json.r
+++ data_from_json.r
@@ -58,3 +58,57 @@ library(jsonlite)
 
 metadata2=fromJSON("data_for_metadata.json", flatten=TRUE)
 ids=metadata2[["body"]][1]
+
+library(leaflet)
+library(sp)
+library(rgdal)
+library(raster)
+library(sf)
+setwd("F:/satellite_data_Muenster/MODIS_neu")
+MS_shape=readOGR("stadtgebiet.shp")
+plot(MS_shape)
+crs(MS_shape)
+#transform coordinates to lat lon
+MS_shape=spTransform(x = MS_shape, CRSobj = "+proj=longlat +datum=WGS84")
+crs(MS_shape)
+plot(MS_shape)
+#plot coords of netatmo stations
+coords=metadata_table[,6:8]
+names(coords)=c("Lon", "Lat","ID")
+coords_test=SpatialPoints(coords = coords[,1:2], proj4string=CRS("+proj=longlat +datum=WGS84"))
+
+plot(coords_test)
+attributes(coords_test)=as.list(coords[,3])
+#MS_shape = spTransform(MS_shape, "+init=epsg:4326")
+plot(MS_shape)
+
+#test plotting coordiantes
+leaflet(MS_shape) %>%
+  addPolygons() %>%
+  addTiles() %>%
+  addMarkers(data=coords, lng = ~Lon, lat = ~Lat)
+
+subset <- coords_test[MS_shape, ]
+plot(subset)
+ID_in_MS=coords$ID[intersect(coords[,1], subset@coords[,1])]
+test.subset=intersect(coords[,1], subset@coords[,1])
+
+test.subset2=intersect(coords[,1], subset@coords[,1])
+ID=data.frame("IDs"=rep(NA, length(test.subset)), "Lon"=test.subset, "Lat"=rep(NA, length(test.subset)))
+for(i in test.subset){
+  ID$IDs[ID$Lon==i]=coords$ID[coords$Lat==i]
+  ID$Lat[ID$Lon==i]=coords$Lat[coords$Lat==i]
+}
+
+write.table(ID_in_MS, "netatmo_ids.csv", sep=";", dec=".")
+
+#get unix time to download data
+start.date=as.numeric(as.POSIXct("2019-08-01", format="%Y-%m-%d"))
+end.time=as.numeric(as.POSIXct("2019-08-30", format="%Y-%m-%d"))
+
+#try to download data from first station
+#70:ee:50:13:29:da
+#timelapse 30 min
+#1378418400
+#temperature
+#1567116000
