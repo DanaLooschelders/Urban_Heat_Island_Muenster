3fafd18b919856eeba482cd37fcbf6e6b2c33fcf
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-05-16 17:24:32 +0200

    test plot

--- comp_wind_temp_diff.r
+++ comp_wind_temp_diff.r
@@ -5,6 +5,13 @@ str(wind)
 beginn=min(Temp_diff_data_frame$Date)
 ende=max(Temp_diff_data_frame$Date)
 wind_subset=wind[wind$MESS_DATUM>beginn&wind$MESS_DATUM<ende,]
+#check
 range(wind_subset$MESS_DATUM, na.rm=T)
 range(Temp_diff_data_frame$Date)
-plot(Temp_diff_data_frame$Date, )
+
+#plot 
+plot(Temp_diff_data_frame$Date, Temp_diff_data_frame$Promenade_1, 
+     type="l", 
+     ylim=c(min(wind$wind_speed, Temp_diff_data_frame$Promenade_1, na.rm=T), max(wind_subset$wind_speed, Temp_diff_data_frame$Georgskommende_1, na.rm=T)))
+lines(wind_subset$MESS_DATUM, wind_subset$wind_speed, col="red")
+
