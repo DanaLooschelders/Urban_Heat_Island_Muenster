8f3c9e35cfdcb6b2b77779071f80a323f4d2d4b9
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-05-16 12:01:16 +0200

    write loop

--- plot_statistics.R
+++ plot_statistics.R
@@ -3,17 +3,23 @@ library(grid)
 library(gridExtra)
 #plot 24 h stats
 #overview plots
+setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit/Plots/stats_plot_mms")
 ggplot(bind_rows(list_iButton_24h_mms, .id="df"), aes(date, mean, colour=df)) +
   geom_line()+theme_bw()+ylab("mean Temperature [°C]")+xlab("Date")+ labs(color='mean values') 
+ggsave(filename=paste("overview_mean_plot",  
+                      list_iButton_24h_mms[[1]][1,1],".jpg")) #name plot with date
 
 ggplot(bind_rows(list_iButton_24h_mms, .id="df"), aes(date, median, colour=df)) +
   geom_line()+theme_bw()+ylab("median Temperature [°C]")+xlab("Date")+ labs(color='median values') 
+ggsave(filename=paste("overview_median_plot",  
+                      list_iButton_24h_mms[[1]][1,1],".jpg")) #name plot with date
 
 ggplot(bind_rows(list_iButton_24h_mms, .id="df"), aes(date, sd, colour=df)) +
   geom_line()+theme_bw()+ylab("sd of Temperature [°C]")+xlab("Date")+ labs(color='standard deviation values') 
+ggsave(filename=paste("overview_sd_plot",  
+                      list_iButton_24h_mms[[1]][1,1],".jpg")) #name plot with date
 
 #plot every logger with mean, meadian and sd
-setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit/Plots/stats_plot_mms")
 
 for (i in 1:length(list_iButton_24h_mms)){
 ggplot(list_iButton_24h_mms[[i]], aes(x=date))+
@@ -46,16 +52,20 @@ ggsave(filename=paste("day_night_stats_plot",
                       names(list_iButton_24h_mms)[i],".jpg")) #name plot with logger name, date
 }
 #gridded plot for stats and data
-stats_plot=ggplot(list_iButton_24h_mms[[1]], aes(date, mean))+ #daily mean
+for (i in 1:length(list_iButton_24h_mms)){
+stats_plot=ggplot(list_iButton_24h_mms[[i]], aes(date, mean))+ #daily mean
   geom_line()+
-  geom_line(data = list_iButton_day_mms[[1]], aes(date, mean))+ #daytime mean
-  geom_line(data = list_iButton_night_mms[[1]], aes(date))+ #nighttime mean
+  geom_line(data = list_iButton_day_mms[[i]], aes(date, mean))+ #daytime mean
+  geom_line(data = list_iButton_night_mms[[i]], aes(date))+ #nighttime mean
   labs(title=paste("mean temperature of logger", names(list_iButton_24h_mms)[1]),
        x="Date", y="Temperature [°C]")+
   theme_bw()+
   geom_ribbon(aes(ymax=mean + sd, ymin=mean-sd), alpha=0.2, colour=NA) #standard deviation
 
-data_plot=ggplot(data=list_iButton_corr_tidy_date[[1]], aes(Datetime.1, Temperature_C_w_off))+
+data_plot=ggplot(data=list_iButton_corr_tidy_date[[i]], aes(Datetime.1, list_iButton_corr_tidy_date[[i]][,3]))+
   geom_line() #plot corrected logger data
-
-grid.arrange(stats_plot, data_plot, ncol=1) #plot both graphs vertically stacked
+grid_plot=grid.arrange(stats_plot, data_plot, ncol=1) #plot both graphs vertically stacked
+ggsave(filename=paste("grid_stats_plot",  
+                      list_iButton_24h_mms[[i]][1,1], "logger",
+                      names(list_iButton_24h_mms)[i],".jpg"), plot =grid_plot ) #name plot with logger name, date
+}
