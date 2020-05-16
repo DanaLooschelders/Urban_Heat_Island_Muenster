38de6ebbd034f876f4947879baaca5bb6b0435ba
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-05-16 20:12:55 +0200

    plot corr wind speed and diff between green and grey infrastructure

--- comp_wind_temp_diff.r
+++ comp_wind_temp_diff.r
@@ -1,3 +1,4 @@
+setwd("C:/00_Dana/Uni/6. Semester/Bachelorarbeit/Plots/weather")
 #plot wind with temperature difference 
 str(Temp_diff_data_frame)
 str(wind)
@@ -9,9 +10,12 @@ wind_subset=wind[wind$MESS_DATUM>beginn&wind$MESS_DATUM<ende,]
 range(wind_subset$MESS_DATUM, na.rm=T)
 range(Temp_diff_data_frame$Date)
 
-#plot 
-plot(Temp_diff_data_frame$Date, Temp_diff_data_frame$Promenade_1, 
+#plot for every place
+for (i in 2:length(Temp_diff_data_frame)-1){ #plot for every column except last column which contains date
+  png(filename = paste("wind_diff_correlation",substring(Temp_diff_data_frame[1,7], first=1, last=10),names(Temp_diff_data_frame)[i],".png"))
+  plot(Temp_diff_data_frame$Date, Temp_diff_data_frame[,i], 
      type="l", 
-     ylim=c(min(wind$wind_speed, Temp_diff_data_frame$Promenade_1, na.rm=T), max(wind_subset$wind_speed, Temp_diff_data_frame$Georgskommende_1, na.rm=T)))
+     ylim=c(range(wind$wind_speed, Temp_diff_data_frame[,i], na.rm=T)))
 lines(wind_subset$MESS_DATUM, wind_subset$wind_speed, col="red")
-
+dev.off()
+}
