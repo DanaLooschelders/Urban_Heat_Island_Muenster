c5edcdad1231b12d0916fd98fee807c05d52c07d
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-05-17 23:31:29 +0200

    Outline QAQC Netatmo

---
+++ QAQC_Netatmo.r
@@ -0,0 +1,13 @@
+#QAQC Netatmo (Meier, Fenner et al. 2017 - 
+#Crowdsourcing air temperature from citizen science 
+#weather stations for urban climate research)
+
+#Data Quality Level A -inconsistent Metadata
+#filter out stations with no lat/lon value
+#Check if stations have identical metadata (remove if >2)
+
+#Data Quality Level B - identify real outdoor measurements
+
+#Data Quality Level C - filter systematic/single radiative errors
+
+#Data Quality Level D - remaining outliers
