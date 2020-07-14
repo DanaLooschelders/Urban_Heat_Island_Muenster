fb50e6f901d19f2116da1bb52b85525e39aeb1ac
DanaLooschelders <dana.looschelders@uni-muenster.de>
2020-07-14 20:59:11 +0200

    tidy up scripts

--- master_script.R
+++ master_script.R
@@ -11,13 +11,13 @@ source("~/Urban_Heat_Island_Muenster/Logger/iButtons-Check_total_DL_from_07.06.2
 #execute all scripts in that order for 2019
 source("~/Urban_Heat_Island_Muenster/Logger/start_time_correction.R") #sets all logger to same starting point
 source("~/Urban_Heat_Island_Muenster/Logger/QAQC_Logger.r") #corrects offset (lab test), spikes, outliers and plots histogram
-source("~/Urban_Heat_Island_Muenster/Logger/plot_data_in_pairs.R") #group the logger (GBS)
-source("~/Urban_Heat_Island_Muenster/Logger/calculate_differences_green_blue.R") #plots
-source("~/Urban_Heat_Island_Muenster/Logger/water_data_plot-r.R")
-source("~/Urban_Heat_Island_Muenster/Logger/veg_sealed_data_plot.r")
-source("~/Urban_Heat_Island_Muenster/Logger/water_air_temp_difference.r")
-source("~/Urban_Heat_Island_Muenster/Logger/split_day_night.r")
-source("~/Urban_Heat_Island_Muenster/Logger/plot_data.r")
+source("~/Urban_Heat_Island_Muenster/Logger/plot_data_in_pairs.R") #group the logger (GBS) and plot together
+source("~/Urban_Heat_Island_Muenster/Logger/plot_differences_green_blue.R") #plot difference between grey and green and export results to csv.file
+source("~/Urban_Heat_Island_Muenster/Logger/plot_water_data.R") #plot all water logger together
+source("~/Urban_Heat_Island_Muenster/Logger/plot_veg_sealed_data.r") #plot all vegetation/sealed logger together
+source("~/Urban_Heat_Island_Muenster/Logger/plot_water_air_difference.r") #plot the difference between air and water temperature
+source("~/Urban_Heat_Island_Muenster/Logger/split_day_night.r") #split the data into day and night (without dawn/dusk)
+source("~/Urban_Heat_Island_Muenster/Logger/plot_split_data.r") #plot the day and night data
 
 ##execute all scripts in that order for 2020 (different logger IDs)
 
@@ -28,12 +28,12 @@ source("~/Urban_Heat_Island_Muenster/Logger/tidy_data_30_min.R")
 
 #for stats (don't always execute) for 2019
 source("~/Urban_Heat_Island_Muenster/Logger/tidy_for_linearity_test.R")
-source("~/Urban_Heat_Island_Muenster/Logger/time_series_plot.R")
-source("~/Urban_Heat_Island_Muenster/Logger/time_series_decomposition_correlation.R")
-source("~/Urban_Heat_Island_Muenster/Logger/time_series_significance_test.R")
-source("~/Urban_Heat_Island_Muenster/Logger/mean_sd_logger.r")
-source("~/Urban_Heat_Island_Muenster/Logger/plot_statistics.R")
-source("~/Urban_Heat_Island_Muenster/Logger/linaer_reg_site_parameters_GSI.R")
+source("~/Urban_Heat_Island_Muenster/Logger/time_series_plot.R") #plot the decomposed time series
+source("~/Urban_Heat_Island_Muenster/Logger/time_series_decomposition_correlation.R") 
+source("~/Urban_Heat_Island_Muenster/Logger/time_series_significance_test.R") #signficance test between logger pairs and for all grey against green logger
+source("~/Urban_Heat_Island_Muenster/Logger/descriptive_stats.r") #calculate mean, median and sd for 24h/day/night 
+source("~/Urban_Heat_Island_Muenster/Logger/plot_descriptive_stats.R") #plots with mean, meadian, standard deviation
+source("~/Urban_Heat_Island_Muenster/Logger/linaer_reg_site_parameters_GSI.R") #lm and cor for site chracteristics/temperatur median 
 source("~/Urban_Heat_Island_Muenster/Logger/pca_site_parameters.R")
 
 #for stats (don't always execute) for 2020 (different logger IDs)
